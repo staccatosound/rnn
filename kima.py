c62f63cd9b04eebdee2f7e1c8459dda91fc9e0ff
Nirin Suarod <nirin.suarod@gmail.com>
2018-05-09 08:23:34 +0900

    Savepoint
    Signed-off-by: Nirin Suarod <nirin.suarod@gmail.com>

--- kima.py
+++ kima.py
@@ -2,81 +2,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Reading CSV file into training set
-training_set = pd.read_csv('data/TSLA_Train.csv')
-
-# Getting relevant feature
-training_set = training_set.iloc[:,1:2]
-
-# Converting to 2D array
-training_set = training_set.values
-
-# Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler()
-training_set = sc.fit_transform(training_set)
-
-# Getting the inputs and the ouputs
-len_train = len(training_set)
-
-X_train = training_set[0:len_train - 1]
-y_train = training_set[1:len_train]
-
-# Example
-today = pd.DataFrame(X_train[0:5])
-tomorrow = pd.DataFrame(y_train[0:5])
-ex = pd.concat([today, tomorrow], axis=1)
-ex.columns = (['today', 'tomorrow'])
-
-# Reshaping into required shape for Keras
-X_train = np.reshape(X_train, (len_train - 1, 1, 1))
-
-# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 
-# Initializing the Recurrent Neural Network
-regressor = Sequential()
-
-# Adding the input layer and the LSTM layer
-regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
-
-# Adding the output layer
-regressor.add(Dense(units = 1))
-
-# Compiling the Recurrent Neural Network
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
-# Fitting the Recurrent Neural Network to the Training set
-regressor.fit(X_train, y_train, batch_size = 32, epochs = 200)
+def train():
+    training_set = pd.read_csv('data/TSLA_Train.csv')
+    training_set = training_set.iloc[:,1:2]
+    training_set = training_set.values
+    training_set = training_set[1:]
 
 
-###############################test############################################
-test_set = pd.read_csv('data/TSLA_Test.csv')
-test_set = test_set[:30]
-real_stock_price = test_set.iloc[:,1:2]
-real_stock_price = real_stock_price.values
-len_test = len(real_stock_price)
+    sc = MinMaxScaler()
+    training_set = sc.fit_transform(training_set)    
+    len_train = len(training_set)
+    
+    X_train = training_set[0:len_train - 4]
+    y_train = training_set[4:len_train]
+    
+#    today = pd.DataFrame(X_train[0:5])
+#    tomorrow = pd.DataFrame(y_train[0:5])
+#    ex = pd.concat([today, tomorrow], axis=1)
+#    ex.columns = (['today', 'tomorrow'])
+    
+    X_train = np.reshape(X_train, ( int((len_train - 1) / 4), 1, 4))
+    y_train = np.reshape(y_train, ( int((len_train - 1) / 4), 1, 4))
+    y_train = [np.sum(a) for a in y_train]
+    regressor = Sequential()
+    regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 4)))
+    regressor.add(Dense(units = 1))
+    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    regressor.fit(X_train, y_train, batch_size = 32, epochs = 200)
 
-n = 29
-# Getting the predicted stock price of 2017
-inputs = real_stock_price[:len_test]
-inputs = sc.transform(inputs)
-inputs = np.reshape(inputs, (len_test, 1, 1))
-predicted_stock_price = regressor.predict(inputs)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price)
-predicted_stock_price = np.insert(predicted_stock_price, 0, 200)
 
-#for i in range(0, n):
-#    print(real_stock_price[i], predicted_stock_price[i], real_stock_price[i + 1])    
+def test():
+    test_set = pd.read_csv('data/TSLA_Test.csv')
+    test_set = test_set[1:33]
+    real_stock_price = test_set.iloc[:,1:2]
+    real_stock_price = real_stock_price.values
+    len_test = len(real_stock_price)
+    
+    # Getting the predicted stock price of 2017
+    X_test = real_stock_price[0:len_test - 4]
+    y_test = training_set[4:len_test]
+    
+#    X_test = sc.transform(X_test)
+    X_test = np.reshape(X_test, (int((len_test - 1) / 4), 1, 4))
+    y_test = np.reshape(y_test, ( int((len_test - 1) / 4), 1, 4))
+    y_test = [np.sum(a) for a in y_test]
 
-# Visualizing the results
-plt.figure(figsize=(10,10),  dpi=100)
-plt.plot(real_stock_price, color = 'red', label = 'Real Price')
-plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Price')
-plt.xlabel('Days')
-plt.ylabel('Price')
-plt.show()
+    print(X_test)
+    y_predict = regressor.predict(X_test)
+#    y_predict = sc.inverse_transform(y_predict)
+#    y_predict = np.insert(y_predict, 0, 200)
+    
+    #for i in range(0, n):
+    #    print(real_stock_price[i], predicted_stock_price[i], real_stock_price[i + 1])    
+    
+    # Visualizing the results
+    plt.figure(figsize=(10,10),  dpi=100)
+    plt.plot(real_stock_price, color = 'red', label = 'Real Price')
+    plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Price')
+    plt.xlabel('Days')
+    plt.ylabel('Price')
+    plt.show()
 
+def main():    
+    train()
+    test()
+
+
+if __name__ == '__main__':
+    main()
